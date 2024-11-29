a9d2a00aa1d3276963888ff0f808f00cc36877f2
springik <springik.games@email.cz>
2024-11-29 01:33:16 +0100

    fix: vercel pg

--- server/utils/db.instance.ts
+++ server/utils/db.instance.ts
@@ -3,12 +3,15 @@ import { Sequelize } from "sequelize";
 const config = useRuntimeConfig()
 const seqConfig = {
     dialect: 'postgres',
-    logging: false, dialectOptions:
+    dialectModuel: require('pg'),
+    logging: false,
+    /*dialectOptions:
     {
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
-    } 
+    }
+        */
 }
 export const sequelize = new Sequelize(`${config.DATABASE_URL}`, seqConfig)
