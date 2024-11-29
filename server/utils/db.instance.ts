dbcd4d743269cb9b5b6b302e7902f92dd4903f03
springik <springik.games@email.cz>
2024-11-29 21:50:08 +0100

    fix: sequelize config

--- server/utils/db.instance.ts
+++ server/utils/db.instance.ts
@@ -5,7 +5,7 @@ const config = useRuntimeConfig()
 const seqConfig = {
     dialect: 'postgres',
     dialectModule: pg,
-    logging: false/*,
+    logging: false,
     dialectOptions:
     {
         ssl: {
@@ -13,6 +13,5 @@ const seqConfig = {
             rejectUnauthorized: false
         }
     }
-        */
 }
 export const sequelize = new Sequelize(`${config.DATABASE_URL}`, seqConfig)
